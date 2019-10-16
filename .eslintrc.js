502b88ce0990eb074d8c39519bba4edeb50cf758
vannvan <862670198@qq.com>
2019-10-16 14:04:35 +0800

    配置完善

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     "commonjs": true,
     "es6": true
   },
-  "extends": "standard", 
+  "extends": "standard",
   "parserOptions": {
     "sourceType": "module"
   },
@@ -13,6 +13,7 @@ module.exports = {
     "linebreak-style": ["error", "unix"],
     "quotes": ["error", "double"],
     "semi": ["error", "always"],
-    "import/no-duplicates": [0]
+    "import/no-duplicates": [0],
+    "no-unused-vars":'off'   //去掉未使用声明报错
   }
 };
--- app/pages/index/index.js
+++ app/pages/index/index.js
@@ -1,13 +1,18 @@
 // import { cloneDeep, throttle, debounce } from "lodash-es";
-// import $ from "jquery";
+import $ from "jquery";
 // import "reset.css";
-// import "@/assets/styles/mixin.scss";
+import "@/assets/styles/mixin.scss";
 import "./index.scss";
 
 import "./test.js";
 
 console.log("首页打印的lallalalalala");
 // console.log(cloneDeep, throttle, debounce, $);
+
+$(".cli-btn").click(function () {
+  console.log("点击事件");
+  window.location.href = "page1.html";
+});
 
 if (module.hot) {
   module.hot.accept("./test.js", function () {
